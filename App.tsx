12a16fece886464c9430a542d50d13f902570d38
GW <kowonp@gmail.com>
2022-04-18 14:43:59 +0900

    Day22: AppLoading

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+const App = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const startAsync = async (): Promise<void> => {
+    const fontsArray: { [x: string]: any }[] = [Ionicons.font];
+    const fontPromise: Promise<void>[] = fontsArray.map((font) => Font.loadAsync(font));
+    const imagesArray: any[] = [require("./assets/coffee.png")];
+    const imagePromise = imagesArray.map((image) => Asset.loadAsync(image));
+    await Promise.all<Promise<void> | Promise<Asset[]>>([...fontPromise, ...imagePromise]);
+  };
+
+  const onFinish = () => {
+    console.log("onFinish");
+    setLoading(false);
+  };
+
+  const onError = () => {
+    console.log("onError");
+  };
+
+  return loading === true ? (
+    <AppLoading startAsync={startAsync} onFinish={onFinish} onError={onError} />
+  ) : (
+    <View>
+      <Text>Hello!!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
